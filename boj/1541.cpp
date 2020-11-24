7bedee5e3f73dd1fb7c6932157c42c2bd41ad673
wlsh44 <k87913j@naver.com>
2020-11-25 01:32:54 +0900

    fix 1541

--- boj/1541.cpp
+++ boj/1541.cpp
@@ -1,48 +1,32 @@
 #include <iostream>
-#include <cctype>
-#include <deque>
 
 using namespace std;
 
 string expression;
 string tmp;
-deque<int> num;
-deque<char> op;
-int sum1;
-int sum2;
+int sum;
+bool flag;
 
 void solve() {
 	cin >> expression;
 	for (auto c : expression) {
-		if (isdigit(c)) {
-			tmp += c;
-		} else {
-			if (c == '+' || c == '-') {
-				op.push_back(c);
+		if (c == '+' || c == '-') {
+			if (!flag) {
+				sum += stoi(tmp);
+				if (c == '-')
+					flag = true;
+			} else {
+				sum -= stoi(tmp);
 			}
-			num.push_back(stoi(tmp));
 			tmp.clear();
+		} else {
+			tmp += c;
 		}
 	}
-	num.push_back(stoi(tmp));
-	sum1 += num.front();
-	num.pop_front();
-
-	while (num.size() != 0) {
-		if (op.front() == '+') {
-			sum1 +=	num.front();
-			op.pop_front();
-			num.pop_front();
-		}
-		else if (op.front() == '-'){
-			do {
-			sum2 += num.front();
-			op.pop_front();
-			num.pop_front();
-			} while (op.front() == '+' || !op.empty());
-		}
-	}
-	cout << sum1 - sum2;
+	if (flag)
+		cout << sum - stoi(tmp);
+	else
+		cout << sum + stoi(tmp);
 }
 
 int main() {
