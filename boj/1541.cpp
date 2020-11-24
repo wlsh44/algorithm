8de8b64617e996d7f00d28012c0a4cda838f8b5c
wlsh44 <k87913j@naver.com>
2020-11-25 01:17:30 +0900

    solve 1541

--- boj/1541.cpp
+++ boj/1541.cpp
@@ -1,15 +1,48 @@
 #include <iostream>
 #include <cctype>
-#include <string>
+#include <deque>
 
 using namespace std;
 
 string expression;
-vector<int> v;
-
+string tmp;
+deque<int> num;
+deque<char> op;
+int sum1;
+int sum2;
 
 void solve() {
+	cin >> expression;
+	for (auto c : expression) {
+		if (isdigit(c)) {
+			tmp += c;
+		} else {
+			if (c == '+' || c == '-') {
+				op.push_back(c);
+			}
+			num.push_back(stoi(tmp));
+			tmp.clear();
+		}
+	}
+	num.push_back(stoi(tmp));
+	sum1 += num.front();
+	num.pop_front();
 
+	while (num.size() != 0) {
+		if (op.front() == '+') {
+			sum1 +=	num.front();
+			op.pop_front();
+			num.pop_front();
+		}
+		else if (op.front() == '-'){
+			do {
+			sum2 += num.front();
+			op.pop_front();
+			num.pop_front();
+			} while (op.front() == '+' || !op.empty());
+		}
+	}
+	cout << sum1 - sum2;
 }
 
 int main() {
