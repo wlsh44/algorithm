f1365ab39bcaf528cdef51a791c114e509414984
wlsh44 <k87913j@gmail.com>
2021-07-10 02:31:19 +0900

    14501

--- boj/_14501.java
+++ boj/_14501.java
@@ -19,20 +19,28 @@ public class _14501 {
             st = new StringTokenizer(br.readLine());
             T[i] = Integer.parseInt(st.nextToken());
             P[i] = Integer.parseInt(st.nextToken());
-            if (T[i] + i > N + 1) {
-                P[i] = 0;
-            }
+//            if (T[i] + i > N + 1) {
+//                P[i] = 0;
+//            }
         }
 
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= i; j++) {
-                if (T[j] + j <= i) {
-                    dp[i] = Math.max(P[i] + dp[j], dp[i]);
-                } else {
-                    dp[i] = Math.max(P[i], dp[i]);
-                }
+//        for (int i = 1; i <= N; i++) {
+//            for (int j = 1; j <= i; j++) {
+//                if (T[j] + j <= i) {
+//                    dp[i] = Math.max(P[i] + dp[j], dp[i]);
+//                } else {
+//                    dp[i] = Math.max(P[i], dp[i]);
+//                }
+//            }
+//        }
+        int max = 0;
+        for (int i = 1; i <= N + 1; i++) {
+            dp[i] = Math.max(dp[i], max);
+            if (T[i] + i <= N + 1) {
+                dp[T[i] + i] = Math.max(dp[i] + P[i], dp[T[i] + i]);
             }
+            max = Math.max(dp[i], max);
         }
-        System.out.println(Arrays.stream(dp).max().getAsInt());
+        System.out.println(max);
     }
 }
