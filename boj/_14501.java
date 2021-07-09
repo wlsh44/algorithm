02cea0cabe539a2bd2dffd633ecca628ea2c5685
wlsh44 <k87913j@gmail.com>
2021-07-09 20:17:59 +0900

    14501

---
+++ boj/_14501.java
@@ -0,0 +1,38 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class _14501 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int[] T = new int[16];
+        int[] P = new int[16];
+        int[] dp = new int[16];
+
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            T[i] = Integer.parseInt(st.nextToken());
+            P[i] = Integer.parseInt(st.nextToken());
+            if (T[i] + i > N + 1) {
+                P[i] = 0;
+            }
+        }
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= i; j++) {
+                if (T[j] + j <= i) {
+                    dp[i] = Math.max(P[i] + dp[j], dp[i]);
+                } else {
+                    dp[i] = Math.max(P[i], dp[i]);
+                }
+            }
+        }
+        System.out.println(Arrays.stream(dp).max().getAsInt());
+    }
+}
