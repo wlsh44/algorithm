66ad737f3bd073e4aa5762f6a803bf10e2b9594a
wlsh44 <k87913j@gmail.com>
2022-09-11 11:59:42 +0900

    1966

--- boj/1966.py
+++ boj/1966.py
@@ -8,26 +8,17 @@ T = int(input())
 for _ in range(T):
     N, M = map(int, input().split())
     arr = deque(list(map(int, input().split())))
-    priority = [0] * 10
-    for num in arr:
-        priority[num] += 1
 
     index = M
     res = 0
-    flag = False
-    for i in range(9, 0, -1):
-        priority_cnt = priority[i]
-        while priority_cnt != 0:
-            if arr[0] == i:
-                res += 1
-                if index == 0:
-                    flag = True
-                    break
-                priority_cnt -= 1
-                arr.popleft()
-            else:
-                arr.rotate(-1)
-            index = index - 1 if index != 0 else len(arr) - 1
-        if flag:
-            print(res)
-            break
+    while True:
+        if arr[0] == max(arr):
+            res += 1
+            if index == 0:
+                print(res)
+                break
+            arr.popleft()
+        else:
+            arr.rotate(-1)
+        index = index - 1 if index != 0 else len(arr) - 1
+
