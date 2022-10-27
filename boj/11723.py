c4c155a806ea9d2f15a23ddad57d8a8ca590f543
wlsh44 <k87913j@gmail.com>
2022-10-27 23:31:26 +0900

    11723

---
+++ boj/11723.py
@@ -0,0 +1,27 @@
+import sys
+
+input = sys.stdin.readline
+
+s = [0] * 21
+
+M = int(input())
+
+for _ in range(M):
+    cmd = input()
+
+    if cmd == "all":
+        s = [1] * 21
+    elif cmd == "empty":
+        s = [0] * 21
+    else:
+        cmd, num = cmd.split()
+        num = int(num)
+        if cmd == "add":
+            s[num] = 1
+        elif cmd == "remove":
+            s[num] = 0
+        elif cmd == "check":
+            print(s[num])
+        elif cmd == "toggle":
+            s[num] = 1 if s[num] == 0 else 0
+
