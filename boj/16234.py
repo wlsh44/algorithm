c3fc194ba11a8cb200e07a10a7dbabf0f83b96f5
wlsh44 <k87913j@gmail.com>
2023-07-08 18:45:21 +0900

    14940

--- boj/16234.py
+++ boj/16234.py
@@ -11,15 +11,18 @@ def bfs(y, x, visited, union):
     q = deque([(y, x)])
     visited[y][x] = True
 
+    people = 0
     while q:
         y, x = q.popleft()
 
+        people += world[y][x]
         for dy, dx in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
             ny, nx = y + dy, x + dx
             if 0 <= ny < N and 0 <= nx < N and L <= abs(world[y][x] - world[ny][nx]) <= R and visited[ny][nx] is False:
                 union.append((ny, nx))
                 visited[ny][nx] = True
                 q.append((ny, nx))
+    return people // len(union)
 
 
 res = 0
@@ -27,22 +30,15 @@ flag = True
 while flag:
     flag = False
     visited = [[False] * N for _ in range(N)]
-    unions = []
     for i in range(N):
         for j in range(N):
             if visited[i][j] is False:
                 union = [(i, j)]
-                bfs(i, j, visited, union)
+                people = bfs(i, j, visited, union)
                 if len(union) != 1:
                     flag = True
-                    unions.append(union)
-    for union in unions:
-        people = 0
-        for y, x in union:
-            people += world[y][x]
-        people //= len(union)
-        for y, x in union:
-            world[y][x] = people
+                for y, x in union:
+                    world[y][x] = people
     if flag:
         res += 1
 print(res)
