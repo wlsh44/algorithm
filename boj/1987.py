dea1871e026f5ac3e149c0dc814b275abbee8efc
wlsh44 <k87913j@gmail.com>
2023-09-01 21:08:26 +0900

    1987

--- boj/1987.py
+++ boj/1987.py
@@ -10,17 +10,18 @@ d = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 num = 0
 visited = {graph[0][0]}
 
-def dfs(y, x, visited, depth):
+
+def dfs(y, x, depth):
     global num
 
     num = max(num, depth)
-    for dy, dx in d:
-        ny, nx = y + dy, x + dx
+    for i in range(4):
+        ny, nx = y + d[i][0], x + d[i][1]
         if 0 <= ny < R and 0 <= nx < C and graph[ny][nx] not in visited:
             visited.add(graph[ny][nx])
-            dfs(ny, nx, visited, depth + 1)
+            dfs(ny, nx, depth + 1)
             visited.remove(graph[ny][nx])
 
 
-dfs(0, 0, visited, 1)
+dfs(0, 0, 1)
 print(num)
