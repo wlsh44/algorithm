131821860c04287f8fb76f373824a5078023134f
wlsh44 <k87913j@gmail.com>
2023-10-23 22:46:23 +0900

    10798

--- boj/10798.py
+++ boj/10798.py
@@ -1,12 +1,6 @@
-arr = []
-for i in range(5):
-    arr.append([None] * 15)
-    row = list(input())
-    for j in range(len(row)):
-        arr[i][j] = row[j]
+arr = [input() for i in range(5)]
 
 for i in range(15):
     for j in range(5):
-        if arr[j][i] == None:
-            continue
-        print(arr[j][i], end="")
+        if i < len(arr[j]):
+            print(arr[j][i], end="")
