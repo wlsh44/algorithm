2bf071ae9dbc6b3009506f689578bffa80e4f5d3
wlsh44 <k87913j@gmail.com>
2026-04-14 08:43:48 +0900

    5397

--- boj/5397.py
+++ boj/5397.py
@@ -26,3 +26,26 @@ for _ in range(T):
         else:
             s.append(c)
     print("".join(s) + "".join(q))
+
+# ---
+from collections import deque
+
+T = int(input())
+
+for _ in range(T):
+    arr = input()
+    s1 = []
+    s2 = deque([])
+    for c in arr:
+        if c == '<':
+            if s1:
+                s2.appendleft(s1.pop())
+        elif c == '>':
+            if s2:
+                s1.append(s2.popleft())
+        elif c == '-':
+            if s1:
+                s1.pop()
+        else:
+            s1.append(c)
+    print(''.join(s1) + ''.join(s2))
